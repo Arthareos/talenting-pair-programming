b4d6bb8007a69896484deea4a09493647382b159
Ciuca Alexandra <maria_alex14@yahoo.com>
2021-07-28 11:54:55 +0300

    ShoutingGreet

--- TDD Hello/Application.Library/Greeter.cs
+++ TDD Hello/Application.Library/Greeter.cs
@@ -11,10 +11,42 @@ namespace Application.Library
 
             if (names.Length == 1) return $"Hello, {names[0]}.";
 
-            List<string> lowercaseNames;
-            List<string> uppercaseNames;
+            List<string> lowercaseNames = SeparatorLowerCase(names);
+            List<string> uppercaseNames = SeparatorUpperCase(names);
+
+            if (names.Length == 1 && uppercaseNames.Contains(names[0])) return $"HELLO, {names[0]}."; 
 
             return "Not implemented yet";
+        }
+
+        public static List<string> SeparatorLowerCase(string[] names)
+        {
+            List<string> lowercaseNames = new();
+
+            foreach(string name in names)
+            {
+                if(!name.ToUpper().Equals(name))
+                {
+                    lowercaseNames.Add(name);
+                }
+            }
+
+            return lowercaseNames;
+        }
+
+        public static List<string> SeparatorUpperCase(string[] names)
+        {
+            List<string> uppercaseNames = new();
+
+            foreach (string name in names)
+            {
+                if (!name.ToLower().Equals(name))
+                {
+                    uppercaseNames.Add(name);
+                }
+            }
+
+            return uppercaseNames;
         }
     }
 }
