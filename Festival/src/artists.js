01ef9082796179013e9b2807a6aa7c01e54da260
Simone Di Fonzo <difonzosimone@hotmail.com>
2021-08-25 16:17:57 +0300

    Style mini-update
     - FIXED: deleted another redundant style.
     - FIXED: optimized script code.
     - FIXED: bug with missing "colina.jpg" image.

--- Festival/src/artists.js
+++ Festival/src/artists.js
@@ -1,23 +1,3 @@
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {
-	scrollFunction()
-};
-
-function scrollFunction() {
-
-	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-		$('#btn_top').css('display', 'block');
-	} else {
-		$('#btn_top').css('display', 'none');
-	}
-
-}
-
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-
 $(document).ready(function () {
   
     $.getJSON("./src/artists.json", function (data) {
